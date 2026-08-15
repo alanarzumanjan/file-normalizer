f61130ff78a52559957a382239681c9c23ca4c33
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-15 23:37:34 +0300

    Update example output format in print_example function

--- src/filenorm/notification.py
+++ src/filenorm/notification.py
@@ -4,4 +4,4 @@ def print_error(message):
     print(f"\033[91m[ERROR]\033[0m {message}", file=sys.stderr)
 
 def print_example(message):
-   print(f"\033[92m[EXAMPLE]\033[0m {message}", file=sys.stderr) 
+   print(f"\033[92mExample:\033[0m {message}", file=sys.stderr) 
