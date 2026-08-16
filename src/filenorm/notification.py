5490e457775f9f7821fafbdaeb95f2634421b1d7
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-08-16 14:18:54 +0300

    Refactor print_error and print_example functions for improved readability

--- src/filenorm/notification.py
+++ src/filenorm/notification.py
@@ -1,7 +1,15 @@
 import sys
 
-def print_error(message):    
-    print(f"\033[91m[ERROR]\033[0m {message}", file=sys.stderr)
+
+def print_error(message):
+    print(
+        f"\033[91m[ERROR]\033[0m {message}",
+        file=sys.stderr,
+    )
+
 
 def print_example(message):
-   print(f"\033[92mExample:\033[0m {message}", file=sys.stderr) 
+    print(
+        f"\033[92mExample:\033[0m {message}",
+        file=sys.stderr,
+    )
